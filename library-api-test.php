ce563307735030ec63a7e2293bac460359208b03
tulsiramk <tulsiram@prudour.com>
2023-01-11 17:11:51 +0530

    testing files

--- library-api-test.php
+++ library-api-test.php
@@ -28,11 +28,142 @@
         .error, .recap_error{
             color:red;
         }
+        #get_toc_from_api_button button{
+            background-color: green;
+            color: #fff;
+            padding:10px;
+            border:none;
+            border-radius: 5px;
+            box-shadow: 4px 3px 7px 2px #5252528c;
+            font-size: 18px;
+            font-weight: 800;
+        }
     </style>
 </head>
 <body>
+    <div id="toc_cnt">
+    <pre class="with-wrap">
+        <p>1. Global Chickpeas Market Introduction</p>
+        <p>1.1. Definition<br>
+        1.2. Taxonomy<br>
+        1.3. Research Scope</p>
+        <p>2. Executive Summary</p>
+        <p>2.1. Key Findings by Major Segments<br>
+        2.2. Top strategies by Major Players</p>
+        <p>3. Global Chickpeas Market Overview</p>
+        <p>3.1. Global Chickpeas Market Dynamics</p>
+        <p>3.1.1. Drivers<br>
+        3.1.2. Opportunities<br>
+        3.1.3. Restraints<br>
+        3.1.4. Challenges</p>
+        <p>3.2. PESTLE Analysis<br>
+        3.3. Opportunity Map Analysis<br>
+        3.4. PORTER’S Five Forces Analysis<br>
+        3.5. Market Competition Scenario Analysis<br>
+        3.6. Product Life Cycle Analysis<br>
+        3.7. Opportunity Orbits<br>
+        3.8. Manufacturer Intensity Map</p>
+        <p>4. Global Chickpeas Market Value ((US$ Mn)), Share (%), and Growth Rate (%) Comparison by Type, 2012-2028</p>
+        <p>4.1. Global Chickpeas Market Analysis by Type: Introduction<br>
+        4.2. Market Size and Forecast by Region<br>
+        4.3. Kabuli Chickpeas<br>
+        4.4. Desi Chickpeas</p>
+        <p>5. Global Chickpeas Market Value ((US$ Mn)), Share (%), and Growth Rate (%) Comparison by Application, 2012-2028</p>
+        <p>5.1. Global Chickpeas Market Analysis by Application: Introduction<br>
+        5.2. Market Size and Forecast by Region<br>
+        5.3. Supplier<br>
+        5.4. Distributers<br>
+        5.5. Retailer</p>
+        <p>6. Global Chickpeas Market Value ((US$ Mn)), Share (%), and Growth Rate (%) Comparison by Region, 2012-2028</p>
+        <p>6.1. North America</p>
+        <p>6.1.1. North America Global Chickpeas Market: Regional Trend Analysis</p>
+        <p>6.1.1. 1.US<br>
+        6.1.1. 2.Canada<br>
+        6.1.1. 3.Mexico</p>
+        <p>6.2. Europe</p>
+        <p>6.2.1. Europe Global Chickpeas Market: Regional Trend Analysis</p>
+        <p>6.2.1.1. Germany<br>
+        6.2.1.2. France<br>
+        6.2.1.3. UK<br>
+        6.2.1.4. Russia<br>
+        6.2.1.5. Italy<br>
+        6.2.1.6. Rest of Europe</p>
+        <p>6.3. Asia-Pacific</p>
+        <p>6.3.1. Asia-Pacific Global Chickpeas Market: Regional Trend Analysis</p>
+        <p>6.3.1.1. China<br>
+        6.3.1.2. Japan<br>
+        6.3.1.3. Korea<br>
+        6.3.1.4.India<br>
+        6.3.1.5.Rest of Asia</p>
+        <p>6.4. Latin America</p>
+        <p>6.4.1. Latin America Global Chickpeas Market: Regional Trend Analysis</p>
+        <p>6.4.1.1. Brazil<br>
+        6.4.1.2. Argentina<br>
+        6.4.1.3. Rest of Latin America</p>
+        <p>6.5. Middle East and Africa</p>
+        <p>6.5.1. Middle East and Africa Global Chickpeas Market: Regional Trend Analysis</p>
+        <p>6.5.1.1. GCC<br>
+        6.5.1.2. South Africa<br>
+        6.5.1.3. Israel<br>
+        6.5.1.4. Rest of MEA</p>
+        <p>7. Global Chickpeas Market Competitive Landscape, Market Share Analysis, and Company Profiles</p>
+        <p>7.1. Market Share Analysis<br>
+        7.2. Company Profiles<br>
+        7.3. Wimmera Grain</p>
+        <p>7.3.1. Company Overview<br>
+        7.3.2. Financial Highlights<br>
+        7.3.3. Product Portfolio<br>
+        7.3.4. SWOT Analysis<br>
+        7.3.5. Key Strategies and Developments</p>
+        <p>7.4. Bean Growers</p>
+        <p>7.4.1. Company Overview<br>
+        7.4.2. Financial Highlights<br>
+        7.4.3. Product Portfolio<br>
+        7.4.4. SWOT Analysis<br>
+        7.4.5. Key Strategies and Developments</p>
+        <p>7.5. Arbel</p>
+        <p>7.5.1. Company Overview<br>
+        7.5.2. Financial Highlights<br>
+        7.5.3. Product Portfolio<br>
+        7.5.4. SWOT Analysis<br>
+        7.5.5. Key Strategies and Developments</p>
+        <p>7.6. Isik Tarim</p>
+        <p>7.6.1. Company Overview<br>
+        7.6.2. Financial Highlights<br>
+        7.6.3. Product Portfolio<br>
+        7.6.4. SWOT Analysis<br>
+        7.6.5. Key Strategies and Developments</p>
+        <p>7.7. JOVA Graneros</p>
+        <p>7.7.1. Company Overview<br>
+        7.7.2. Financial Highlights<br>
+        7.7.3. Product Portfolio<br>
+        7.7.4. SWOT Analysis<br>
+        7.7.5. Key Strategies and Developments</p>
+        <p>7.8. Mast Qalander</p>
+        <p>7.8.1. Company Overview<br>
+        7.8.2. Financial Highlights<br>
+        7.8.3. Product Portfolio<br>
+        7.8.4. SWOT Analysis<br>
+        7.8.5. Key Strategies and Developments</p>
+        <p>7.9. Indraprasth foods</p>
+        <p>7.9.1. Company Overview<br>
+        7.9.2. Financial Highlights<br>
+        7.9.3. Product Portfolio<br>
+        7.9.4. SWOT Analysis<br>
+        7.9.5. Key Strategies and Developments</p>
+        <p>7.10. OLEGA</p>
+        <p>7.10.1. Company Overview<br>
+        7.10.2. Financial Highlights<br>
+        7.10.3. Product Portfolio<br>
+        7.10.4. SWOT Analysis<br>
+        7.10.5. Key Strategies and Developments</p>
+        <p>8. Assumptions and Acronyms<br>
+        9. Research Methodology<br>
+        10. Contact</p>
+    </pre>
+    </div>
     <div id="get_toc_from_api_button"></div>
-    <br/>
+    
 
   
 
